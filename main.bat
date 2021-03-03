799d8b5ab225f604a6d0e067ee9553d7c0c5fdfa
dotX12 <64792903+dotX12@users.noreply.github.com>
2021-03-03 07:09:10 +0300

    Create bat file

---
+++ main.bat
@@ -0,0 +1,87 @@
+@ECHO OFF
+set "calculator_value=0.00"
+set "file_path=%1"
+
+GOTO :get_max_decimal_len
+GOTO :EOF
+
+
+
+:get_max_decimal_len
+SETLOCAL EnableDelayedExpansion
+IF NOT EXIST "%file_path%" ECHO File not found & GOTO OUT
+FOR /F "skip=2" %%i IN ('FIND "." "%file_path%"') DO (
+	CALL :len %%~i
+)
+
+GOTO :file_calculator %decimals_len%
+
+
+:file_calculator
+SETLOCAL EnableDelayedExpansion
+IF NOT EXIST "%file_path%" ECHO File not found & GOTO OUT
+FOR /F "skip=2" %%b IN ('find "." "%file_path%"') DO (
+	CALL :add_zeros %%b %decimals_len% RESULT
+	CALL :calculator !RESULT! value
+)
+ECHO Calculation result - %calculator_value%
+ENDLOCAL
+GOTO :EOF
+
+:slice
+set full_integer=%~1
+set decimal_part=%full_integer:*.=%
+call set whole_part=%%full_integer:.%decimal_part%=%%
+set %2=%whole_part%
+set %3=%decimal_part%
+
+GOTO :EOF
+
+:len
+SET l=0
+CALL :slice %1 whole_part decimal_part
+
+
+:len_loop
+SET x=!decimal_part:~%l%,1!
+IF NOT DEFINED x (ENDLOCAL & CALL :set_max %l% & GOTO :EOF)
+SET /a l=%l%+1
+GOTO :len_loop
+
+
+:set_max
+SET /a len = %1
+IF %len% GTR %decimals_len% SET /A decimals_len = %len%
+GOTO :EOF
+
+
+:add_zeros
+CALL :slice %1 whole decimal
+SET /a "integer=%decimal%"
+SET /a count_zero=%2
+SET "output_string=!integer!"
+
+
+CALL :len %integer% len
+SET /a start_for = %len%+1
+FOR /L %%d IN (%start_for%, 1, %count_zero%) DO (
+  SET output_string=!output_string!0)
+
+SET %3=%whole%.%output_string%
+GOTO :EOF
+
+
+:calculator
+SET "numA=%calculator_value%"
+SET "numB=%1"
+SET "fpA=%numA:.=%"
+SET "fpB=%numB:.=%"
+SET /A add=fpA+fpB
+echo %numA% + %numB% = !add:~0,-%decimals_len%!.!add:~-%decimals_len%!
+SET "value=!add:~0,-%decimals_len%!.!add:~-%decimals_len%!"
+SET "calculator_value=%value%"
+
+GOTO :EOF
+
+:OUT
+pause>nul
